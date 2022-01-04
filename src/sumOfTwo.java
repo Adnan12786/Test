3ace66432744c8dc77042ef98f621eec33604f3e
Adnan Bhas <bhasadnan27@gmail.com>
2022-01-04 13:37:14 +0530

    prg2

---
+++ src/sumOfTwo.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class SumOfTwo {
+    public static void main(String[] args) {
+        int x, y, sum;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter First Number : ");
+        x = sc.nextInt();
+        System.out.println("Enter Second Number : ");
+        y = sc.nextInt();
+        sum = sum(x, y);
+        System.out.println("The sum of two numbers x and y is: " + sum);
+    }
+
+
+    public static int sum(int a, int b)
+    {
+        return a+b;
+    }
+}
